37a6fa71688a43ece754730f126f124cf2020c60
Saint99599 <saint99599@gmail.com>
2023-05-25 02:02:16 +0700

    fix follow map

--- src/Camera.cs
+++ src/Camera.cs
@@ -12,7 +12,7 @@ namespace Project1.src
         public Matrix Follow(Rectangle target)
         {
             target.X = MathHelper.Clamp(target.X, (int)Game1.screenWidth / 2, (int)(1000-Game1.screenHeight/ 2));
-            target.Y = (int)Game1.screenHeight/2;
+            //target.Y = (int)Game1.screenHeight/2;
             Vector3 translation = new Vector3(-target.X - target.Width/2, -target.Y - target.Height/2, 0);
 
             Vector3 offset = new Vector3(Game1.screenWidth/2, Game1.screenHeight/ 2, 0);
